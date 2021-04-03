eed7d16f8be863bf80a3908353e231b53abc0c58
Claudiu C. Bogdan <claudiu.engineer@gmail.com>
2021-04-03 20:31:10 +0300

    feat: add register api call

--- app/src/main/java/ro/code4/deurgenta/services/ApiInterface.kt
+++ app/src/main/java/ro/code4/deurgenta/services/ApiInterface.kt
@@ -3,10 +3,15 @@ package ro.code4.deurgenta.services
 import io.reactivex.Observable
 import retrofit2.http.Body
 import retrofit2.http.POST
+import ro.code4.deurgenta.data.model.Register
 import ro.code4.deurgenta.data.model.User
 import ro.code4.deurgenta.data.model.response.LoginResponse
+import ro.code4.deurgenta.data.model.response.RegisterResponse
 
 interface ApiInterface {
     @POST("")
     fun login(@Body user: User): Observable<LoginResponse>
+
+    @POST("register")
+    fun register(@Body data: Register): Observable<RegisterResponse>
 }
--- app/src/main/java/ro/code4/deurgenta/ui/register/RegisterViewModel.kt
+++ app/src/main/java/ro/code4/deurgenta/ui/register/RegisterViewModel.kt
@@ -1,12 +1,39 @@
 package ro.code4.deurgenta.ui.register
 
-import android.app.Application
-import android.content.SharedPreferences
+import android.util.Log
+import androidx.lifecycle.LiveData
+import io.reactivex.Observable
 import org.koin.core.inject
+import ro.code4.deurgenta.data.model.Register
+import ro.code4.deurgenta.data.model.response.RegisterResponse
+import ro.code4.deurgenta.helper.Result
+import ro.code4.deurgenta.helper.SingleLiveEvent
+import ro.code4.deurgenta.repositories.Repository
 import ro.code4.deurgenta.ui.base.BaseViewModel
+import ro.code4.deurgenta.ui.login.LoginActivity
 
 class RegisterViewModel : BaseViewModel() {
-    private val app: Application by inject()
-    private val preferences: SharedPreferences by inject()
+
+    private val repository: Repository by inject()
+
+    private val registerLiveData = SingleLiveEvent<Result<Class<*>>>()
+
+    fun registered(): LiveData<Result<Class<*>>> = registerLiveData
+
+    fun register(data: Register): Observable<RegisterResponse> {
+
+        Log.d("TEST", "SEND ${data.firstName} .to users.")
+        return repository.register(data)
+    }
+
+    fun onRegisterSuccess() {
+        // TODO: change login activity with register success fragment
+        val nextActivity = LoginActivity::class.java
+        registerLiveData.postValue(Result.Success(nextActivity))
+    }
+
+    fun onRegisterFail(error: Throwable, message: String = "") {
+        registerLiveData.postValue(Result.Failure(error, message))
+    }
 
 }
