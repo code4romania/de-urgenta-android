21f95c9df80cc733d7a454270df8573c130428b7
Claudiu C. Bogdan <claudiu.engineer@gmail.com>
2021-04-03 20:31:10 +0300

    feat: add register api call

--- app/src/main/java/ro/code4/deurgenta/ui/register/RegisterActivity.kt
+++ app/src/main/java/ro/code4/deurgenta/ui/register/RegisterActivity.kt
@@ -1,13 +1,19 @@
 package ro.code4.deurgenta.ui.register
 
 import android.os.Bundle
+import android.os.Debug
 import android.text.Html
 import android.text.method.LinkMovementMethod
-import kotlinx.android.synthetic.main.activity_login.*
+import android.util.Log
+import androidx.lifecycle.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_register.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import ro.code4.deurgenta.R
+import ro.code4.deurgenta.data.model.Register
 import ro.code4.deurgenta.ui.base.BaseAnalyticsActivity
+import ro.code4.deurgenta.helper.startActivityWithoutTrace
 
 class RegisterActivity : BaseAnalyticsActivity<RegisterViewModel>() {
 
@@ -20,12 +26,28 @@ class RegisterActivity : BaseAnalyticsActivity<RegisterViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         clickListenersSetup()
+        registerUserObservable()
     }
 
     private fun clickListenersSetup() {
+
+        submitButton.setOnClickListener {
+            val registerData = getRegisterData()
+            viewModel
+                .register(registerData)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    viewModel.onRegisterSuccess()
+                }, {
+                    val errorMessage = getString(R.string.error_generic)
+                    viewModel.onRegisterFail(it, errorMessage)
+                })
+        }
+
         termsAndConditionsSetup()
+
     }
 
     private fun termsAndConditionsSetup() {
@@ -38,4 +60,33 @@ class RegisterActivity : BaseAnalyticsActivity<RegisterViewModel>() {
 
     }
 
+    private fun getRegisterData(): Register {
+
+        val firstName = firstNameEditText.text.toString()
+        val lastName = lastNameEditText.text.toString()
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+
+        return Register(
+            firstName,
+            lastName,
+            email,
+            password
+        )
+    }
+
+    private fun registerUserObservable() {
+        viewModel.registered().observe(this, Observer {
+            it.handle(
+                onSuccess = { activity ->
+                    activity?.let(::startActivityWithoutTrace)
+                },
+                onFailure = { error ->
+                    showDefaultErrorSnackBar(submitButton)
+
+                    submitButton.isEnabled = true
+                }
+            )
+        })
+    }
 }
