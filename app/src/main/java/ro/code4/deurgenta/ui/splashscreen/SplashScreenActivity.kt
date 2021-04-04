17f3ec12138cd091c9b042c3e00e3b5029e730e2
lukstbit <lukstbit@gmail.com>
2021-04-04 12:37:28 +0300

    Implement the splashscreen UI

--- app/src/main/java/ro/code4/deurgenta/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/ro/code4/deurgenta/ui/splashscreen/SplashScreenActivity.kt
@@ -1,7 +1,10 @@
 package ro.code4.deurgenta.ui.splashscreen
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import androidx.lifecycle.Observer
+import android.view.View
+import android.view.animation.LinearInterpolator
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import ro.code4.deurgenta.R
 import ro.code4.deurgenta.helper.startActivityWithoutTrace
@@ -9,7 +12,6 @@ import ro.code4.deurgenta.ui.base.BaseAnalyticsActivity
 import ro.code4.deurgenta.ui.login.LoginActivity
 import ro.code4.deurgenta.ui.main.MainActivity
 import ro.code4.deurgenta.ui.register.RegisterActivity
-import ro.code4.deurgenta.widget.ProgressDialogFragment
 
 class SplashScreenActivity : BaseAnalyticsActivity<SplashScreenViewModel>() {
 
@@ -18,31 +20,28 @@ class SplashScreenActivity : BaseAnalyticsActivity<SplashScreenViewModel>() {
     override val screenName: Int
         get() = R.string.analytics_title_splash
 
-    private val progressDialog: ProgressDialogFragment by lazy {
-        ProgressDialogFragment().also {
-            it.isCancelable = false
-        }
-    }
     override val viewModel: SplashScreenViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        progressDialog.showNow(supportFragmentManager, ProgressDialogFragment.TAG)
-
-        viewModel.loginLiveData().observe(this, Observer { loginStatus ->
-            progressDialog.dismiss()
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+        val logoAnimator = ObjectAnimator.ofFloat(logo, "rotation", 0f, 360f)
+        with(logoAnimator) {
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.RESTART
+            duration = 2000
+            interpolator = LinearInterpolator()
+            start()
+        }
+        viewModel.loginStatus.observe(this) { loginStatus ->
             val activity = when {
-                loginStatus.isLoggedIn && loginStatus.onboardingCompleted -> MainActivity::class.java
-                loginStatus.isLoggedIn -> RegisterActivity::class.java
+                loginStatus.isUserLoggedIn && loginStatus.onboardCompleted -> MainActivity::class.java
+                loginStatus.isUserLoggedIn -> RegisterActivity::class.java
                 else -> LoginActivity::class.java
             }
-
             startActivityWithoutTrace(activity)
-        })
-    }
-
-    override fun onDestroy() {
-        if (progressDialog.isResumed) progressDialog.dismissAllowingStateLoss()
-        super.onDestroy()
+        }
     }
 }
--- app/src/main/java/ro/code4/deurgenta/ui/splashscreen/SplashScreenViewModel.kt
+++ app/src/main/java/ro/code4/deurgenta/ui/splashscreen/SplashScreenViewModel.kt
@@ -2,36 +2,26 @@ package ro.code4.deurgenta.ui.splashscreen
 
 import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
-import org.koin.core.inject
 import ro.code4.deurgenta.helper.SingleLiveEvent
 import ro.code4.deurgenta.helper.getToken
 import ro.code4.deurgenta.helper.hasCompletedOnboarding
 import ro.code4.deurgenta.ui.base.BaseViewModel
 
-class SplashScreenViewModel : BaseViewModel() {
-    private val sharedPreferences: SharedPreferences by inject()
-    private val loginLiveData = SingleLiveEvent<LoginStatus>()
+class SplashScreenViewModel(sharedPreferences: SharedPreferences) : BaseViewModel() {
 
-    fun loginLiveData(): LiveData<LoginStatus> = loginLiveData
+    private val _loginStatus = SingleLiveEvent<LoginStatus>()
+    val loginStatus: LiveData<LoginStatus> = _loginStatus
 
     init {
-        checkLogin()
-    }
-
-    private fun checkLogin() {
-        val isLoggedIn = sharedPreferences.getToken() != null
-
-        loginLiveData.postValue(
-            LoginStatus(
-                isLoggedIn,
-                sharedPreferences.hasCompletedOnboarding()
-            )
+        val isUserLoggedIn = sharedPreferences.getToken() != null
+        _loginStatus.value = LoginStatus(
+            isUserLoggedIn,
+            sharedPreferences.hasCompletedOnboarding()
         )
     }
-
-    data class LoginStatus(
-        val isLoggedIn: Boolean,
-        val onboardingCompleted: Boolean
-    )
-
 }
+
+data class LoginStatus(
+    val isUserLoggedIn: Boolean,
+    val onboardCompleted: Boolean
+)
