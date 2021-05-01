c7f8acbe6a436bcb4b285e74bdbdf227b0a58024
Gabriel Majeri <gabriel.majeri6@gmail.com>
2021-05-01 17:49:39 +0300

    Implement add group fragment (#45)
    * Add `BaseAnalyticsFragment` constructor with a given layout ID
    
    * Add group creation fragments
    
    * Remove hardcoded dimensions and styles
    
    * Link onboarding navigation to group creation navigation
    
    Co-authored-by: Irina Borozan <anirib@gmail.com>

---
+++ app/src/main/java/ro/code4/deurgenta/ui/group/GroupCreateInfoFragment.kt
@@ -0,0 +1,27 @@
+package ro.code4.deurgenta.ui.group
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.fragment_group_create_info.*
+import ro.code4.deurgenta.R
+import ro.code4.deurgenta.ui.base.BaseAnalyticsFragment
+
+class GroupCreateInfoFragment : BaseAnalyticsFragment(R.layout.fragment_group_create_info) {
+    override val screenName: Int = R.string.analytics_title_group
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        input_group_name.addTextChangedListener {
+            // Allow submission only if name is not blank
+            button_create_new_group_continue.isEnabled = !it.isNullOrEmpty()
+        }
+
+        button_create_new_group_continue.setOnClickListener {
+            val name = input_group_name.text.toString()
+
+            // TODO: create new group with the given name
+        }
+    }
+}
