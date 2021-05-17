9d1314fc3c71dacc1868b2a6b27122bd1c9d4e0c
Ancuta Popescu <s_ancuta@yahoo.com>
2021-05-17 21:32:47 +0300

    Implement main settings fragment (code4romania#42)

--- app/src/main/java/ro/code4/deurgenta/ui/settings/SettingsFragment.kt
+++ app/src/main/java/ro/code4/deurgenta/ui/settings/SettingsFragment.kt
@@ -14,27 +14,17 @@ import retrofit2.http.HTTP
 import ro.code4.deurgenta.BuildConfig
 import ro.code4.deurgenta.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [SettingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val contactEmailUri by lazy {
+        Uri.fromParts(    "mailto",
+            BuildConfig.SUPPORT_EMAIL,
+            null    )
     }
 
     override fun onCreateView(
@@ -47,18 +37,30 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        report_problem.setOnClickListener {
+        report_problem.setOnClickListener {reportProblemClicked(it)}
+    /* report_problem.setOnClickListener {
             Intent(Intent.ACTION_SEND).apply {
                 // The intent does not have a URI, so declare the "text/plain" MIME type
                 type = "text/html"
                 putExtra(Intent.EXTRA_EMAIL, arrayOf("jan@example.com")) // recipients
                 putExtra(Intent.EXTRA_SUBJECT, "Email subject")
                 putExtra(Intent.EXTRA_TEXT, "Email message text")
-                putExtra(Intent.EXTRA_STREAM, Uri.parse("content://path/to/email/attachment"))
-                // You can also attach multiple items by passing an ArrayList of Uris
             }
-        }
+        }*/
     }
 
+    private fun reportProblemClicked(view: View?) {
+        val emailIntent = Intent(
+                Intent.ACTION_SENDTO,
+                contactEmailUri
+        )
+        startActivity(
+                Intent.createChooser(
+                        emailIntent,
+                        "Send email via"
+                )
+        )
+
+    }
 
 }
