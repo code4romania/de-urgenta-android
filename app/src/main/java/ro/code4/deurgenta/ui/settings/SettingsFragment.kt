1aec8f15a85e7bf18aeebc4178970730b2b529c8
Ancuta Popescu <s_ancuta@yahoo.com>
2021-05-19 10:00:17 +0300

    Implement main settings fragment (#42) (#51)
    * Implement main settings fragment (#42)
    
    * Implement main settings fragment (code4romania#42)
    
    * Implement main settings fragment (code4romania#42)
    
    * Implement main settings fragment (code4romania#42)
    
    * https://github.com/ancutapopescu/de-urgenta-android.git
    
    * Implement main settings fragment (code4romania#42)

---
+++ app/src/main/java/ro/code4/deurgenta/ui/settings/SettingsFragment.kt
@@ -0,0 +1,52 @@
+package ro.code4.deurgenta.ui.settings
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_about.*
+import kotlinx.android.synthetic.main.fragment_settings.*
+import retrofit2.http.HTTP
+import ro.code4.deurgenta.BuildConfig
+import ro.code4.deurgenta.R
+
+class SettingsFragment : Fragment() {
+
+    private val contactEmailUri by lazy {
+        Uri.fromParts(    "mailto",
+            BuildConfig.SUPPORT_EMAIL,
+            null)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        report_problem.setOnClickListener {reportProblemClicked(it)}
+    }
+
+    private fun reportProblemClicked(view: View?) {
+        val emailIntent = Intent(
+                Intent.ACTION_SENDTO,
+                contactEmailUri
+        )
+        startActivity(
+                Intent.createChooser(
+                        emailIntent,
+                        getString(R.string.report_problem)
+                )
+        )
+
+    }
+
+}
